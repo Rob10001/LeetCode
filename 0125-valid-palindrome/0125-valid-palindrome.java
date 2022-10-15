892d7ee1df40ea07b4bec022b0ea184e06992b9a
Rob10001 <73569726+Rob10001@users.noreply.github.com>
2022-10-15 13:36:17 -0500

    Time: 7 ms (64.09%), Space: 43.1 MB (86.97%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,13 +1,23 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s = s.trim().toLowerCase();
-        String alphNum = "";
-        for(int i = 0; i < s.length(); i++) 
-            if(Character.isLetterOrDigit(s.charAt(i))) 
-                alphNum += s.substring(i, i+1);
-        for(int i = 0; i < alphNum.length(); i++)
-            if(alphNum.charAt(i) != alphNum.charAt(alphNum.length() - 1 - i))
-                return false;
-        return true;
+       s = s.toLowerCase();
+       int left = 0;
+       int right = s.length() - 1;
+       while(left <= right){
+           if(!Character.isLetterOrDigit(s.charAt(left))){
+               left++;
+               continue;
+           }
+           if(!Character.isLetterOrDigit(s.charAt(right))){
+               right--;
+               continue;
+           }
+           if(s.charAt(left) != s.charAt(right)){
+               return false;
+           }
+           left++;
+           right--;
+       }
+       return true;
     }
 }
