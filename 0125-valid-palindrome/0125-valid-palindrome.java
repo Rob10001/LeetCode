786840b3d9d79a840c3b8063ab946faf1eddd445
Rob10001 <73569726+Rob10001@users.noreply.github.com>
2022-10-15 13:35:16 -0500

    Time: 996 ms (22.26%), Space: 419.4 MB (11.89%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,7 +3,7 @@ class Solution {
         s = s.trim().toLowerCase();
         String alphNum = "";
         for(int i = 0; i < s.length(); i++) 
-            if(Character.isAlphabetic(s.charAt(i)) || Character.isDigit(s.charAt(i))) 
+            if(Character.isLetterOrDigit(s.charAt(i))) 
                 alphNum += s.substring(i, i+1);
         for(int i = 0; i < alphNum.length(); i++)
             if(alphNum.charAt(i) != alphNum.charAt(alphNum.length() - 1 - i))
