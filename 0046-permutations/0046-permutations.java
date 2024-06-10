1d1ea5a11185157a7f8079eebddd3713c80f8e8d
Roberto Tamez <73569726+Rob10001@users.noreply.github.com>
2024-06-10 07:56:11 -0500

    Time: 2 ms (43.25%), Space: 45.1 MB (8.94%) - LeetHub

---
+++ 0046-permutations/0046-permutations.java
@@ -0,0 +1,29 @@
+class Solution {
+    public static List<List<Integer>> results = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        boolean[] used = new boolean[nums.length];
+        combos(nums, used, new ArrayList<Integer>());
+        List<List<Integer>> output = new ArrayList<>(results);
+        results.clear();
+        return output;
+    }
+    
+    public static void combos(int[] nums, boolean[] used, ArrayList<Integer> currStack) {
+        for(int i = 0; i < nums.length; i++) {
+            if(!used[i]) {
+                ArrayList<Integer> recurse = new ArrayList<Integer>(currStack);
+                recurse.add(nums[i]);
+                
+                boolean[] recurseUsed = Arrays.copyOf(used, used.length);
+                recurseUsed[i] = true;
+                combos(nums, recurseUsed, recurse);
+            }
+        }
+        
+        boolean goodToGo = true;
+        for(boolean b : used) {
+            if(!b) goodToGo = false;
+        }
+        if(goodToGo) results.add(currStack);
+    }
+}
